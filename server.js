0c01e0c8ef00d345bdfd0c1a8d4c215839663c91
SHorne <swhorne@gmail.com>
2020-09-03 22:22:52 -0600

    Adding Atlas credentials

--- server.js
+++ server.js
@@ -1,27 +1,28 @@
 const express = require("express");
-const logger = require("morgan");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 8080;
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
 
-const app = express();
+const server = express();
 
-app.use(logger("dev"));
+server.use(express.urlencoded({ extended: true }));
+server.use(express.json());
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+server.use(express.static("public"));
 
-app.use(express.static("public"));
+mongoose.connect(MONGODB_URI);
 
-mongoose.connect("mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false
+server.use(require("./routes/api-routes.js"));
+server.use(require("./routes/html-routes.js"));
+
+server.listen(PORT, () => {
+    console.log(
+        "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+        PORT,
+        PORT
+    );
 });
 
-// routes
-app.use(require("./routes/api.js"));
-app.use(require("./routes/view.js"));
 
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
-});
+// mongodb+srv://UofUser:AtlasPW2020@fitness-tracker.0ehxl.mongodb.net/<dbname>?retryWrites=true&w=majority
